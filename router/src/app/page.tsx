844c3f977afbba37f87b5adc7ee13e3717efd99b
Manan <mananarif025@gmail.com>
2024-08-22 03:24:36 +0500

    app-router

--- router/src/app/page.tsx
+++ router/src/app/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 export default function Home(){
   return(
     <>
+
+    <h1>hello mars</h1>
    <Link href={'/About'}>About</Link>
    <br />
    <Link href={'/Contact'}>Contact</Link>
